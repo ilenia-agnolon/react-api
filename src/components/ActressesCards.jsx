208148e9e60884ee4456f9716f44abcb8fb43577
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-10-03 00:01:33 +0200

    MILESTONE3 COMPLETATA: mostrare in pagina una card per ciascun attore, con grafica a piacimento

--- src/components/ActressesCards.jsx
+++ src/components/ActressesCards.jsx
@@ -19,45 +19,41 @@ export default function ActressesCards() {
 
   return (
     <>
-      {/* img attrice */}
-      <div
-        className="single-card-container"
-        style={{
-          backgroundImage:
-            "linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0)), url('./img-prova.jpg')",
-        }}
-      >
-        <div className="text-container">
-          <div className="name-and-bio-container">
+      <div className="cards-container">
+        {/* MAP */}
+        {actresses.map((actress) => (
+          <div
+            key={actress.id}
+            className="single-card"
+            style={{
+              backgroundImage: `linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0)), url(${actress.image})`,
+            }}
+          >
             {/* nome */}
-            <h1>Nome</h1>
+            <h1>{actress.name}</h1>
+
             {/* biografia */}
-            <p>
-              Meryl Streep is considered one of the greatest actresses of her
-              generation and has received numerous accolades for her versatile
-              performances.
-            </p>
+            <p>{actress.biography}</p>
+
             <div className="line"></div>
+            <div className="info-container">
+              <ul>
+                <li>
+                  {/* anno nascita */}
+                  <span>Anno di nascita: {actress.birth_year}</span>
+                </li>
+                <li>
+                  {/* nazionalità */}
+                  <span>Nazionalità: {actress.nationality}</span>
+                </li>
+                <li>
+                  {/* awards */}
+                  <span>Riconoscimenti: {actress.awards}</span>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div className="info-container">
-            <ul>
-              <li>
-                {/* anno nascita */}
-                <span>Anno di nascita: 1966</span>
-              </li>
-              <li>
-                {/* nazionalità */}
-                <span>Nazionalità: American</span>
-              </li>
-              <li>
-                {/* awards */}
-                <span>
-                  Riconoscimenti: 4 Academy Awards, 8 Golden Globe Awards
-                </span>
-              </li>
-            </ul>
-          </div>
-        </div>
+        ))}
       </div>
     </>
   );
